15efa69b1c23b46d3ea00b97ceb643971bb1ec10
diyaa <diyaaine@gmail.com>
2021-02-27 09:50:49 -0500

    added

---
+++ src/com/diyamolvarghese/Gender.java
@@ -0,0 +1,7 @@
+package com.diyamolvarghese;
+
+public enum Gender {
+    MALE;
+    FEMALE
+    OTHER;
+}
---
+++ src/com/diyamolvarghese/Student.java
@@ -0,0 +1,22 @@
+package com.diyamolvarghese;
+
+import javax.xml.soap.SAAJResult;
+import java.sql.Date;
+
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Gender gender;   //Enum
+    private String address;
+    private String city;
+    private String pincode;
+    private String cellPhone;
+    private String email;
+    private String department;
+    private float marks[];
+    private float totalMarks[];
+
+
+}
